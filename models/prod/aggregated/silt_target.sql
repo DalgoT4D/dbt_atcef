45f33f5fd78f601d39ba826be2488f05ac42ea40
Siddhant Singh <ssiddhant3030@gmail.com>
2024-03-15 01:51:58 +0530

    added the logic for two more metrics

---
+++ models/prod/aggregated/silt_target.sql
@@ -0,0 +1,36 @@
+{{ config(
+  materialized='table'
+) }}
+
+
+-- models/silt_differences.sql
+
+WITH silt_calculations AS (
+    SELECT
+        dam,
+        date,
+        district,
+        state,
+        taluka,
+        village,
+        silt_to_be_excavated,
+        silt_target,
+        (silt_to_be_excavated - silt_target) * 1000 AS silt_difference_litres,
+        (silt_to_be_excavated * 1000) / 10000 AS equivalent_water_tankers
+    FROM
+        {{ ref('work_orders') }}
+)
+
+SELECT
+    dam,
+    date,
+    district,
+    state,
+    taluka,
+    village,
+    silt_to_be_excavated,
+    silt_target,
+    silt_difference_litres,
+    equivalent_water_tankers
+FROM
+    silt_calculations
--- models/prod/aggregated/work_order.sql
+++ models/prod/aggregated/work_order.sql
@@ -2,37 +2,11 @@
   materialized='table'
 ) }}
 
-with silt_data as (
-    select
-        dam,
-        district,
-        state,
-        taluka,
-        village,
-        project_status,
-        encounter_location,
-        sum(silt_to_be_excavated) as silt_target_per_dam, -- sum of silt_to_be_excavated per dam
-        sum(total_silt_carted) as silt_excavated_per_dam, -- sum of total_silt_carted per dam
-        max(date_time) as latest_encounter_date -- latest date of silt excavation
-    from {{ ref('work_order_union') }} 
-    where encounter_type = 'Work order daily Recording - Farmer'
-    group by dam, district, state, taluka, village, project_status, encounter_location
-),
 
-progress as (
-    select
-        dam,
-        district,
-        state,
-        taluka,
-        village,
-        project_status,
-        encounter_location,
-        silt_target_per_dam,
-        silt_excavated_per_dam,
-        (silt_excavated_per_dam / nullif(silt_target_per_dam, 0)) * 100 as excavation_progress_percentage,
-        latest_encounter_date
-    from silt_data
-)
-
-select * from progress
+SELECT distinct dam, max(date_time) as date, district, state, taluka, village, max(silt_to_be_excavated) as silt_target, sum(total_silt_carted) as silt_to_be_excavated
+FROM
+	{{ ref('work_order_union') }} 
+WHERE
+	encounter_type = 'Work order daily Recording - Farmer'
+Group by
+dam, district, state, taluka, village
