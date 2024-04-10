870e86ca4505ffe88b3b9adb334bc29a5d2fb68d
Siddhant Singh <ssiddhant3030@gmail.com>
2024-04-10 19:40:49 +0530

    added the date

--- models/prod/final/farmer_mobile_verified.sql
+++ models/prod/final/farmer_mobile_verified.sql
@@ -5,6 +5,7 @@
 WITH DistinctRecords AS (
     SELECT DISTINCT ON (mobile_number)
         mobile_number,
+		date_time,
         mobile_verified,
         encounter_type,
         state,
@@ -25,6 +26,7 @@ SELECT
     taluka,
     village,
     dam,
+	date_time,
     COUNT(CASE WHEN mobile_verified = TRUE THEN 1 ELSE NULL END) AS mobile_verified_count,
     COUNT(CASE WHEN mobile_verified = FALSE THEN 1 ELSE NULL END) AS mobile_unverified_count,
     COUNT(*) AS total -- Counts the total number of records (both verified and unverified)
@@ -35,4 +37,5 @@ GROUP BY
     district,
     taluka,
     village,
-    dam
+    dam,
+	date_time
