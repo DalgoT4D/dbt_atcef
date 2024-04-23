9c27913519e25c8e96f3f1203e6c221ce54e2aff
Siddhant Singh <ssiddhant3030@gmail.com>
2024-04-23 13:16:18 +0530

    fixed machine endline

--- models/prod/aggregated/machine_endline_aggregated.sql
+++ models/prod/aggregated/machine_endline_aggregated.sql
@@ -13,7 +13,7 @@ SELECT
 	type_of_machine,
 	ROUND(sum(total_working_hours_of_machine), 2) as total_working_hours_of_machine
 FROM
-	prod_aggregated.work_order_union
+	{{ref('work_order_union')}}
 WHERE
 	encounter_type = 'Excavating Machine Endline'
 	AND total_working_hours_of_machine IS NOT NULL
