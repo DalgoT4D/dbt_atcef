3c92a0a06d2514edab44837a71b2993dfa0c5d8a
Siddhant Singh <ssiddhant3030@gmail.com>
2024-04-26 11:09:27 +0530

    work order metic

--- models/prod/aggregated/work_order_metric.sql
+++ models/prod/aggregated/work_order_metric.sql
@@ -2,22 +2,27 @@
   materialized='table'
 ) }}
 
-
-SELECT 
-    first_name as work_order,
-    MAX(date_time) as date_time,
-    ngo_name,
-    state, 
-    district,
-    taluka,
-    dam,
-    village,
-    COALESCE(MAX(silt_target), 0) as silt_target, 
-    COALESCE(CAST(ROUND(SUM(CASE WHEN total_silt_carted::text <> 'NaN' THEN total_silt_carted ELSE 0 END)) AS numeric), 0) AS silt_achieved
-    FROM
-    {{ ref('work_order_union') }}
+SELECT
+	MAX(date_time) AS date_time,
+	ngo_name,
+	state,
+	district,
+	taluka,
+	dam,
+	village,
+	COALESCE(MAX(silt_target), 0) AS silt_target,
+	COALESCE(SUM(total_silt_carted), 0) + 
+    COALESCE(MAX(CAST(total_silt_excavated_by_GP_for_non_farm_purpose AS numeric)), 0) AS silt_achieved
+FROM
+	{{ref ('work_order_union')}}
 WHERE
-    encounter_type = 'Work order daily Recording - Farmer'
-    AND project_ongoing = 'Ongoing' and silt_target != 0
+	encounter_type = 'Work order daily Recording - Farmer'
+	AND project_ongoing = 'Ongoing'
+	AND silt_target != 0
 GROUP BY
-    state, district, taluka, dam, village, first_name, ngo_name
+	state,
+	district,
+	taluka,
+	dam,
+	village,
+	ngo_name
