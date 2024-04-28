a25a2c85d7df8c0bc02802b58cee8bccd1d6434e
Siddhant Singh <ssiddhant3030@gmail.com>
2024-04-28 22:50:14 +0530

    progress waterbodies

---
+++ models/prod/final/gdgsgom_2023/progress_waterbodies_gdgs_23.sql
@@ -0,0 +1,19 @@
+{{ config(
+  materialized='table'
+) }}
+
+SELECT DISTINCT ON (dam) dam AS waterbodies,
+       date_time,
+       state, 
+       village, 
+       taluka, 
+       district, 
+       encounter_type,
+       CONCAT(
+          CASE WHEN project_ongoing IS NOT NULL THEN 'Ongoing' ELSE '' END,
+          CASE WHEN project_not_started IS NOT NULL THEN 'Not Started' ELSE '' END,
+          CASE WHEN project_completed IS NOT NULL THEN 'Completed' ELSE '' END
+       ) AS project_status
+FROM {{ ref('work_order_gdgs_2023') }}
+Where dam is not null
+ORDER BY dam, date_time
