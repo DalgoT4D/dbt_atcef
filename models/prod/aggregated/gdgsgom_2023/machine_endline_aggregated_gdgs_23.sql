f263f53498d0ebcf1efb6c13ff0564771636ee8d
Siddhant Singh <ssiddhant3030@gmail.com>
2024-04-28 17:27:55 +0530

    total machine calc

---
+++ models/prod/aggregated/gdgsgom_2023/machine_endline_aggregated_gdgs_23.sql
@@ -0,0 +1,26 @@
+{{ config(
+  materialized='table'
+) }}
+
+
+SELECT
+	max(date_time) as date_time,
+	dam as waterbodies,
+	state, 
+	district,
+	taluka,
+	village,
+	sum(total_working_hours_of_machine) as total_working_hours_of_machine,
+	type_of_machine
+	
+FROM
+	{{ref('work_order_gdgs_2023')}}
+WHERE
+	encounter_type = 'Excavating Machine Endline'
+group by 
+	dam,
+	state, 
+	district,
+	taluka,
+	village,
+	type_of_machine
