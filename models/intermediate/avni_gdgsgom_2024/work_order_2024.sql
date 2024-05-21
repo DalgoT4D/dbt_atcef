f8451a60b8aaec625f9d55c9d8e6c33885525c5d
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-22 00:50:36 +0530

    fixed

--- models/intermediate/avni_gdgsgom_2024/work_order_2024.sql
+++ models/intermediate/avni_gdgsgom_2024/work_order_2024.sql
@@ -2,6 +2,32 @@
   materialized='table'
 ) }}
 
+WITH WorkOrderEncounters AS (
+    SELECT 
+        e.*,
+        s.uid AS subject_uid
+    FROM {{ ref('encounters_2024') }} AS e
+    LEFT JOIN {{ ref('subjects_2024') }} AS s 
+        ON e.subject_id = s.uid 
+        AND e.encounter_type <> 'Work order daily Recording - Farmer'
+        AND s.subject_voided = false
+    LEFT JOIN {{ ref('subjects_2024') }} AS f 
+        ON e.farmer_sub_id = f.uid 
+        AND e.encounter_type = 'Work order daily Recording - Farmer'
+        AND f.subject_voided = false
+    LEFT JOIN {{ ref('subjects_2024') }} AS m 
+        ON e.machine_sub_id = m.uid 
+        AND e.encounter_type = 'Work order daily Recording - Farmer'
+        AND m.subject_voided = false
+    WHERE 
+        (e.encounter_type = 'Work order daily Recording - Farmer' 
+            AND e.farmer_sub_id IS NOT NULL 
+            AND e.machine_sub_id IS NOT NULL 
+            AND f.uid IS NOT NULL 
+            AND m.uid IS NOT NULL)
+        OR (e.encounter_type <> 'Work order daily Recording - Farmer' 
+            AND s.uid IS NOT NULL)
+)
 
 SELECT 
     s.*,
@@ -18,11 +44,9 @@ SELECT
         WHEN woe.encounter_type = 'Work order endline' THEN 'Completed'
         ELSE NULL
     END AS project_completed
-FROM 
-    {{ ref('subjects_2024') }} AS s
-LEFT JOIN 
-    {{ ref('encounters_2024') }} AS woe 
-ON 
-    s.uid = woe.subject_id 
+FROM {{ ref('subjects_2024') }} AS s
+LEFT JOIN WorkOrderEncounters AS woe 
+    ON s.uid = woe.subject_uid
 WHERE 
     s.dam != 'Test'
+
