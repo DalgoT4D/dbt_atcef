796209c17ae5784369a998da6017d4e7101a21d2
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-23 19:27:27 +0530

    fixed

--- models/intermediate/avni_gdgsgom_2024/subjects_2024.sql
+++ models/intermediate/avni_gdgsgom_2024/subjects_2024.sql
@@ -10,8 +10,9 @@ WITH cte AS (
         INITCAP(COALESCE(location->>'Dam')) AS dam,  
         INITCAP(COALESCE(location->>'District')) AS district,  
         CASE  -- Standardize state names
-            WHEN LOWER(location->>'State') LIKE '%maharastra%' THEN 'Maharashtra'
-            ELSE INITCAP(COALESCE(location->>'State'))
+        WHEN LOWER(location->>'State') LIKE '%maharashtra%' THEN 'Maharashtra'
+        WHEN LOWER(location->>'State') LIKE '%maharshatra%' THEN 'Maharashtra'
+        ELSE INITCAP(COALESCE(location->>'State', ''))
         END AS state,
         INITCAP(COALESCE(location->>'Taluka')) AS taluka, 
         INITCAP(COALESCE(location->>'GP/Village')) AS village, 
