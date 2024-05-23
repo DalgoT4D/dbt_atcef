16d8fc80741797171f9bc440c5c0c19db92b3864
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-23 15:10:57 +0530

    gdgs fix

--- models/intermediate/avni_gdgsgom_2023/work_order_gdgs_2023.sql
+++ models/intermediate/avni_gdgsgom_2023/work_order_gdgs_2023.sql
@@ -3,6 +3,28 @@
 ) }}
 
 
+WITH WorkOrderEncounters AS (
+    SELECT 
+        e.*,
+        subject_id as subject_uid
+    FROM {{ ref('encounters_gdgs_2023') }} AS e
+    LEFT JOIN {{ ref('subjects_gdgs_2023') }} AS s 
+        ON e.subject_id = s.uid 
+        AND e.encounter_type <> 'Work order daily Recording - Farmer'
+        AND s.subject_voided = false
+    LEFT JOIN {{ ref('subjects_gdgs_2023') }} AS f 
+        ON e.farmer_sub_id = f.uid 
+        AND e.encounter_type = 'Work order daily Recording - Farmer'
+        AND f.subject_voided = false
+    WHERE 
+        (e.encounter_type = 'Work order daily Recording - Farmer' 
+            AND e.farmer_sub_id IS NOT NULL 
+            AND e.machine_sub_id IS NOT NULL 
+            AND f.uid IS NOT NULL)
+        OR (e.encounter_type <> 'Work order daily Recording - Farmer' 
+            AND s.uid IS NOT NULL)
+)
+
 SELECT 
     s.*,
     woe.*,
@@ -21,8 +43,8 @@ SELECT
 FROM 
     {{ ref('subjects_gdgs_2023') }} AS s
 LEFT JOIN 
-    {{ ref('encounters_gdgs_2023') }} AS woe 
+    WorkOrderEncounters AS woe 
 ON 
-    s.uid = woe.subject_id 
+    s.uid = woe.subject_uid
 WHERE 
     s.dam != 'Test'
--- models/intermediate/avni_gdgsgom_2024/work_order_2024.sql
+++ models/intermediate/avni_gdgsgom_2024/work_order_2024.sql
@@ -3,6 +3,28 @@
 ) }}
 
 
+WITH WorkOrderEncounters AS (
+    SELECT 
+        e.*,
+        subject_id as subject_uid
+    FROM {{ ref('encounters_2024') }} AS e
+    LEFT JOIN {{ ref('subjects_2024') }} AS s 
+        ON e.subject_id = s.uid 
+        AND e.encounter_type <> 'Work order daily Recording - Farmer'
+        AND s.subject_voided = false
+    LEFT JOIN {{ ref('subjects_2024') }} AS f 
+        ON e.farmer_sub_id = f.uid 
+        AND e.encounter_type = 'Work order daily Recording - Farmer'
+        AND f.subject_voided = false
+    WHERE 
+        (e.encounter_type = 'Work order daily Recording - Farmer' 
+            AND e.farmer_sub_id IS NOT NULL 
+            AND e.machine_sub_id IS NOT NULL 
+            AND f.uid IS NOT NULL)
+        OR (e.encounter_type <> 'Work order daily Recording - Farmer' 
+            AND s.uid IS NOT NULL)
+)
+
 SELECT 
     s.*,
     woe.*,
@@ -21,8 +43,9 @@ SELECT
 FROM 
     {{ ref('subjects_2024') }} AS s
 LEFT JOIN 
-    {{ ref('encounters_2024') }} AS woe 
+    WorkOrderEncounters AS woe 
 ON 
-    s.uid = woe.subject_id 
+    s.uid = woe.subject_uid
 WHERE 
     s.dam != 'Test'
+
