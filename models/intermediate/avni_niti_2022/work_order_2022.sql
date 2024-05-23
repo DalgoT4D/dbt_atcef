550aaebd3a0bb1e280bb9ca7d6ddec79b66b8871
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-23 13:57:40 +0530

    added the comment

--- models/intermediate/avni_niti_2022/work_order_2022.sql
+++ models/intermediate/avni_niti_2022/work_order_2022.sql
@@ -2,6 +2,30 @@
   materialized='table'
 ) }}
 
+-- This part of the query creates a CTE named WorkOrderEncounters which will be used later in the main query.
+-- It selects all columns from the table encounter_2022 and renames subject_id to subject_uid.
+-- There are two LEFT JOIN operations:
+-- The first LEFT JOIN joins encounter_2022 (e) with subjects_2022 (s) based on subject_id and filters out records where the encounter type is 'Work order daily Recording - Farmer' and s.subject_voided is false.
+-- The second LEFT JOIN joins encounter_2022 (e) with subjects_2022 (f) based on farmer_sub_id and includes only records where the encounter type is 'Work order daily Recording - Farmer' and f.subject_voided is false.
+-- The WHERE clause filters records based on two conditions:
+-- The first condition includes records where encounter_type is 'Work order daily Recording - Farmer' and farmer_sub_id, machine_sub_id, and f.uid are all not null.
+-- The second condition includes records where encounter_type is not 'Work order daily Recording - Farmer' and s.uid is not null.
+
+-- This part of the query selects all columns from the subjects_2022 table (s) and the CTE WorkOrderEncounters (woe).
+-- It also includes three CASE statements to categorize the projects based on their status:
+-- project_ongoing is set to 'Ongoing' if woe.subject_id is not null and woe.encounter_type is not 'Work order endline'.
+-- project_not_started is set to 'Yet to start' if woe.subject_id is null.
+-- project_completed is set to 'Completed' if woe.encounter_type is 'Work order endline'.
+-- There is a LEFT JOIN between subjects_2022 (s) and the CTE WorkOrderEncounters (woe) on s.uid and woe.subject_uid.
+-- The WHERE clause filters records to include only those where s.dam, s.district, s.taluka, s.state, and s.village are not null and s.subject_voided is false.
+-- Summary
+-- The query is designed to:
+
+-- Create a temporary table (WorkOrderEncounters) that includes encounter records linked to subjects and farmers under certain conditions.
+-- Retrieve all subjects with relevant details, along with their encounter details from the CTE.
+-- Categorize each subject's project status as 'Ongoing', 'Yet to start', or 'Completed' based on the conditions specified in the CASE statements.
+-- Ensure only subjects with valid geographic details and not marked as voided are included in the final result set.
+
 WITH WorkOrderEncounters AS (
     SELECT 
         e.*,
