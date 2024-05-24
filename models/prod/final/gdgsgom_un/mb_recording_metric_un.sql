1022b71897366d58a7fc2e9ddfef171ec1125725
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-24 15:04:36 +0530

    mb recording is done

---
+++ models/prod/final/gdgsgom_un/mb_recording_metric_un.sql
@@ -0,0 +1,22 @@
+{{ config(
+  materialized='table'
+) }}
+
+
+SELECT
+    max(date_time) as date_time,
+    state, 
+    district,
+    taluka,
+    dam,
+    village,
+    SUM(COALESCE(silt_excavated_as_per_MB_recording, 0)) AS silt_excavated_as_per_MB_recording,
+    MAX(COALESCE(silt_to_be_excavated_as_per_plan, 0)) AS total_silt_excavated
+FROM
+   {{ref('work_order_gdgs_union')}}
+group by 
+    state,
+	district,
+	taluka,
+	dam,
+	village
