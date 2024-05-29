62cbbfd71d285dffdccb9b33df77e584f7fd0d73
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-29 22:29:20 +0530

    farmer mobile verified

--- models/prod/aggregated/farmer_endline.sql
+++ models/prod/aggregated/farmer_endline.sql
@@ -39,44 +39,56 @@ SELECT
     taluka,
     village,
     dam AS waterbodies,
-    SUM(
-        CASE
+    SUM(CASE 
+            WHEN mobile_verified = 'True' THEN 1 
+            ELSE 0 
+        END
+    ) AS verified_farmers,
+    SUM(CASE 
+            WHEN mobile_verified = 'False' THEN 1 
+            ELSE 0 
+        END
+    ) AS unverified_farmers,
+    SUM(CASE 
+            WHEN mobile_verified = 'True' THEN 1 
+            ELSE 0 
+        END
+    ) + SUM(CASE 
+            WHEN mobile_verified = 'False' THEN 1 
+            ELSE 0 
+        END
+    ) AS total_farmers,
+    SUM(CASE
             WHEN category_of_farmer = 'Small (2.5-4.99 acres)' THEN 1
             ELSE 0
         END
     ) AS vulnerable_small,
-    SUM(
-        CASE
+    SUM(CASE
             WHEN category_of_farmer = 'Marginal (0-2.49 acres)' THEN 1
             ELSE 0
         END
     ) AS vulnerable_marginal,
-    SUM(
-        CASE
+    SUM(CASE
             WHEN category_of_farmer = 'Semi-medium (5-9.55 acres)' THEN 1
             ELSE 0
         END
     ) AS semi_medium,
-    SUM(
-        CASE
+    SUM(CASE
             WHEN category_of_farmer = 'Medium (10-24.99 acres)' THEN 1
             ELSE 0
         END
     ) AS medium,
-    SUM(
-        CASE
+    SUM(CASE
             WHEN category_of_farmer = 'Large (above 25 acres)' THEN 1
             ELSE 0
         END
     ) AS large
 FROM
     farmer_union
+WHERE LOWER(dam) NOT LIKE '%voided%'
 GROUP BY
     state,
     district,
     taluka,
     village,
     dam
-
-
-
