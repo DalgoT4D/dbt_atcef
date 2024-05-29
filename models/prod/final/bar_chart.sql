052104edd056f5026769ab78911863e8b1573ae4
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-29 09:07:40 +0530

    progress of waterbodies

--- models/prod/final/bar_chart.sql
+++ models/prod/final/bar_chart.sql
@@ -3,19 +3,29 @@
 ) }}
 
 
-SELECT DISTINCT ON (dam) dam AS waterbodies,
-       date_time,
-       state, 
-       village, 
-       taluka, 
-       district, 
-       ngo_name,
-       encounter_type,
-       CONCAT(
-          CASE WHEN project_ongoing IS NOT NULL THEN 'Ongoing' ELSE '' END,
-          CASE WHEN project_not_started IS NOT NULL THEN 'Not Started' ELSE '' END,
-          CASE WHEN project_completed IS NOT NULL THEN 'Completed' ELSE '' END
-       ) AS project_status
-FROM {{ ref('work_order_union') }}
-ORDER BY dam, date_time
+WITH max_dates AS (
+    SELECT
+        dam,
+        MAX(date_time) AS max_date_time
+    FROM {{ ref('work_order_union') }}
+    GROUP BY dam
+)
+
+SELECT DISTINCT ON (w.dam)
+    w.dam AS waterbodies,
+    w.date_time,
+    w.state, 
+    w.village, 
+    w.taluka, 
+    w.district, 
+    w.ngo_name,
+    w.encounter_type,
+    CONCAT(
+        CASE WHEN w.project_ongoing IS NOT NULL THEN 'Ongoing' ELSE '' END,
+        CASE WHEN w.project_not_started IS NOT NULL THEN 'Not Started' ELSE '' END,
+        CASE WHEN w.project_completed IS NOT NULL THEN 'Completed' ELSE '' END
+    ) AS project_status
+FROM {{ ref('work_order_union') }} w
+JOIN max_dates md ON w.dam = md.dam AND w.date_time = md.max_date_time
+ORDER BY w.dam, w.date_time
 
