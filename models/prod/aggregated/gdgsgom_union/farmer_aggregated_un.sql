96450243e2c26c281cc967441d4e873a179072dd
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-30 17:34:23 +0530

    new model added

--- models/prod/aggregated/gdgsgom_union/farmer_aggregated_un.sql
+++ models/prod/aggregated/gdgsgom_union/farmer_aggregated_un.sql
@@ -3,22 +3,33 @@
 ) }}
 
 
-with cte as (SELECT DISTINCT on (first_name, mobile_number, uid)
-  uid,
-  date_time,
-  first_name, 
-  mobile_number,
-  state,
-  district,
-  taluka,
-  village, 
-  dam, 
-  trim(category_of_farmer) as category_of_farmer, 
-  mobile_verified, 
-  encounter_type
-FROM 
-  {{ ref('work_order_gdgs_union') }}
-)
+with farmer_union as (select uid, 
+       date_time, 
+       first_name,
+       mobile_number, 
+       mobile_verified,
+       state, 
+       district, 
+       taluka, 
+       village,
+       dam, 
+       category_of_farmer 
+FROM
+    {{ ref('farmer_gdgs_count_23') }} 
+UNION ALL 
+select uid, 
+       date_time, 
+       first_name,
+       mobile_number, 
+       mobile_verified,
+       state, 
+       district, 
+       taluka, 
+       village,
+       dam, 
+       category_of_farmer 
+FROM
+    {{ ref('farmer_gdgs_count_24') }} )
   
 SELECT
     date_time,
@@ -27,72 +38,49 @@ SELECT
     taluka,
     village,
     dam AS waterbodies,
-    SUM( CASE 
-            WHEN mobile_verified = 'True' THEN 1 
-            else 0 
-         END
-        ) AS verified_farmers,
-    SUM( CASE 
-            WHEN mobile_verified = 'False' THEN 1 
-            else 0 
-         END
-        ) AS unverified_farmers,
     SUM(CASE 
-            WHEN mobile_verified = 'True' OR mobile_verified = 'False' THEN 1 
+            WHEN mobile_verified = 'True' THEN 1 
             ELSE 0 
-        END
-    ) AS total,
-    SUM(
-        CASE
-            WHEN category_of_farmer = 'Small (2.5-4.99 Acres)' THEN 1
+        END) AS verified_farmers,
+    SUM(CASE 
+            WHEN mobile_verified = 'False' THEN 1 
+            ELSE 0 
+        END) AS unverified_farmers,
+    COUNT(*) AS total,
+    SUM(CASE 
+            WHEN LOWER(category_of_farmer) = 'small (2.5-4.99 acres)' THEN 1
             ELSE 0
-        END
-    ) AS vulnerable_small,
-    SUM(
-        CASE
-            WHEN category_of_farmer = 'Marginal (0-2.49 Acres)' THEN 1
+        END) AS vulnerable_small,
+    SUM(CASE 
+            WHEN LOWER(category_of_farmer) = 'marginal (0-2.49 acres)' THEN 1
             ELSE 0
-        END
-    ) AS vulnerable_marginal,
-    SUM(
-        CASE
-            WHEN category_of_farmer = 'Semi-Medium (5 To 9.99 Acre)' THEN 1
+        END) AS vulnerable_marginal,
+    SUM(CASE 
+            WHEN LOWER(category_of_farmer) = 'semi-medium (5 to 9.99 acre)' THEN 1
             ELSE 0
-        END
-    ) AS semi_medium,
-    SUM(
-        CASE
-            WHEN category_of_farmer = 'Medium (10-24.99 Acres)' THEN 1
+        END) AS semi_medium,
+    SUM(CASE 
+            WHEN LOWER(category_of_farmer) = 'medium (10-24.99 acres)' THEN 1
             ELSE 0
-        END
-    ) AS medium,
-    SUM(
-        CASE
-            WHEN category_of_farmer = 'Large (Above 25 Acres)' THEN 1
+        END) AS medium,
+    SUM(CASE 
+            WHEN LOWER(category_of_farmer) = 'large (above 25 acres)' THEN 1
             ELSE 0
-        END
-    ) AS large,
-     SUM(
-        CASE
-            WHEN category_of_farmer = 'Widow' THEN 1
+        END) AS large,
+    SUM(CASE 
+            WHEN LOWER(category_of_farmer) = 'widow' THEN 1
             ELSE 0
-        END
-    ) AS widow,
-      SUM(
-        CASE
-            WHEN category_of_farmer = 'Disabled' THEN 1
+        END) AS widow,
+    SUM(CASE 
+            WHEN LOWER(category_of_farmer) = 'disabled' THEN 1
             ELSE 0
-        END
-    ) AS Disabled,
-      SUM(
-        CASE
-            WHEN category_of_farmer = 'Family Of Farmer Who Committed Suicide' THEN 1
+        END) AS disabled,
+    SUM(CASE 
+            WHEN LOWER(category_of_farmer) = 'family of farmer who committed suicide' THEN 1
             ELSE 0
-        END
-    ) AS family_of_farmer_who_committed_suicide
-    
+        END) AS family_of_farmer_who_committed_suicide
 FROM
-    cte
+    farmer_union
 GROUP BY
     date_time,
     state,
@@ -100,4 +88,3 @@ GROUP BY
     taluka,
     village,
     dam
-
