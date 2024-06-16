f7373a34b2ca3a6735f8d9ad3ff185e5d8dfbc26
Siddhant Singh <ssiddhant3030@gmail.com>
2024-06-17 01:27:01 +0530

    more final models added

---
+++ models/prod/final/gdgs_2023/work_order_metric_gdgs_23.sql
@@ -0,0 +1,20 @@
+{{ config(
+  materialized='table'
+) }}
+
+
+    SELECT 
+        work_order_name,
+        MAX(date_time) as date_time,
+        state, 
+        district,
+        taluka,
+        dam,
+        village,
+        MAX(silt_target) as silt_target,
+        SUM(total_silt_carted) as silt_achieved
+    FROM {{ref('farmer_calc_silt_gdgs_23')}}
+    GROUP BY
+        state, district, taluka, dam, village, work_order_name
+
+
