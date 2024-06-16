3c47d4135eb60ad256abb82a057d25ccc40eca7a
Siddhant Singh <ssiddhant3030@gmail.com>
2024-06-17 01:23:22 +0530

    farmer percentage added

---
+++ models/prod/final/gdgs_2023/farmer_gdgs_2023_percentage.sql
@@ -0,0 +1,46 @@
+{{ config(
+  materialized='table'
+) }}
+
+SELECT
+    state,
+    district,
+    taluka,
+    village,
+    dam,
+    'vulnerable' AS farmer_type,
+    SUM(vulnerable_small + vulnerable_marginal + widow + disabled + family_of_farmer_who_committed_suicide) AS farmers_count
+FROM
+    {{ref('farmer_agg_gdgs_23')}}
+GROUP BY
+    state,
+    district,
+    taluka,
+    village,
+    dam
+
+UNION ALL
+
+SELECT
+    state,
+    district,
+    taluka,
+    village,
+    dam,
+    'others' AS farmer_type,
+    SUM(semi_medium + medium + large) AS farmers_count
+FROM
+    {{ref('farmer_agg_gdgs_23')}}
+GROUP BY
+    state,
+    district,
+    taluka,
+    village,
+    dam
+
+ORDER BY
+    state,
+    district,
+    taluka,
+    village,
+    dam, farmer_type
