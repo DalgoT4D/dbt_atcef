20d43b3a079b815c7ec459f0346298658c2c3619
Siddhant Singh <ssiddhant3030@gmail.com>
2024-06-16 17:00:26 +0530

    gramin data is done

---
+++ models/intermediate/gramin_niti/cleaned/machine_gramin.sql
@@ -0,0 +1,40 @@
+{{ config(
+  materialized='table'
+) }}
+
+
+WITH mycte AS (
+    SELECT
+        "ID" AS machine_id,
+        INITCAP(TRIM(COALESCE(observations->>'First name'))) AS machine_name,
+        observations->>'Type of Machine' AS type_of_machine,
+        INITCAP(COALESCE(location->>'Dam')) AS dam,
+        INITCAP(COALESCE(location->>'District')) AS district,
+        "Subject_type" AS subject_type,
+        CASE
+            WHEN LOWER(location->>'State') LIKE '%maharashtra%' THEN 'Maharashtra'
+            WHEN LOWER(location->>'State') LIKE '%maharshatra%' THEN 'Maharashtra'
+            ELSE INITCAP(COALESCE(location->>'State', ''))
+        END AS state,
+        INITCAP(COALESCE(location->>'Taluka')) AS taluka,
+        INITCAP(COALESCE(location->>'GP/Village')) AS village,
+        "Voided" AS machine_voided
+    FROM
+        {{ source('source_gramin', 'subjects_gramin') }}
+    WHERE
+        "Subject_type" = 'Excavating Machine' AND "Voided" = 'False' and NOT (LOWER(location->>'Dam') ~ 'voided') 
+),
+approval_machines AS (
+    SELECT d.*, a.approval_status AS machine_approval_status
+    FROM mycte d
+    JOIN {{ ref('approval_status_gramin') }} a ON d.machine_id = a.entity_id
+    WHERE a.entity_type = 'Subject' AND a.approval_status = 'Approved'
+)
+
+
+    {{ dbt_utils.deduplicate(
+        relation='approval_machines',
+        partition_by='machine_id',
+        order_by='machine_id desc'
+    ) }}
+
---
+++ models/prod/aggregated/gramin/progress_waterbodies_gramin.sql
@@ -0,0 +1,40 @@
+{{ config(
+  materialized='table'
+) }}
+
+WITH waterbodies AS (
+    SELECT 
+        w.dam,
+        w.work_order_id,
+        e.farmer_sub_id,
+        w.state,
+        w.village,
+        w.district,
+        w.taluka,
+        e.encounter_type,
+        e.date_time,
+        ROW_NUMBER() OVER (PARTITION BY w.dam ORDER BY e.date_time DESC) AS row_num
+    FROM {{ ref('work_order_gramin') }} AS w
+    LEFT JOIN {{ ref('encounters_gramin') }} AS e 
+        ON e.subject_id = w.work_order_id
+    WHERE e.encounter_type = 'Work order daily Recording - Farmer' 
+       OR e.encounter_type = 'Work order endline'
+)
+
+SELECT 
+    dam,
+    work_order_id,
+    farmer_sub_id,
+    state,
+    village,
+    district,
+    taluka,
+    encounter_type,
+    date_time,
+    CASE 
+        WHEN encounter_type = 'Work order daily Recording - Farmer' THEN 'Ongoing'
+        WHEN encounter_type = 'Work order endline' THEN 'Completed'
+        ELSE NULL
+    END AS project_status
+FROM waterbodies
+WHERE row_num = 1
