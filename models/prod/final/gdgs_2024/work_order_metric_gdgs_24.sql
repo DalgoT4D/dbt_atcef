b244b6eabe9a8a081989bbe7a743a086af609b35
Siddhant Singh <ssiddhant3030@gmail.com>
2024-06-17 18:45:07 +0530

    quick fix

--- models/prod/final/gdgs_2024/work_order_metric_gdgs_24.sql
+++ models/prod/final/gdgs_2024/work_order_metric_gdgs_24.sql
@@ -14,6 +14,7 @@
         MAX(silt_target) as silt_target,
         SUM(total_silt_carted) as silt_achieved
     FROM {{ref('farmer_calc_silt_gdgs_24')}}
+    WHERE total_silt_carted::text != 'NaN'
     GROUP BY
         state, district, taluka, dam, village, work_order_name
 
