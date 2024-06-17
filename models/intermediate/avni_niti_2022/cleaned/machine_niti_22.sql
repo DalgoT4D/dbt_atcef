6b438e2ce03da5c9c86e205cec6fc03dee7f59d8
Siddhant Singh <ssiddhant3030@gmail.com>
2024-06-17 14:10:54 +0530

    fixed 2022

--- models/intermediate/avni_niti_2022/cleaned/machine_niti_22.sql
+++ models/intermediate/avni_niti_2022/cleaned/machine_niti_22.sql
@@ -22,39 +22,18 @@ WITH mycte AS (
     FROM
         {{ source('source_atecf_surveyss', 'subjects_2022') }}
     WHERE
-        "Voided" = 'False'
+        "Subject_type" = 'Excavating Machine' AND "Voided" = 'False'
 ),
 approval_machines AS (
     SELECT d.*, a.approval_status AS machine_approval_status
     FROM mycte d
     JOIN {{ ref('approval_statuses_niti_2022') }} a ON d.machine_id = a.entity_id
     WHERE a.entity_type = 'Subject' AND a.approval_status = 'Approved'
-),
-deduplicated_machines AS (
+)
+
+
     {{ dbt_utils.deduplicate(
         relation='approval_machines',
         partition_by='machine_id',
         order_by='machine_id desc'
     ) }}
-)
-SELECT
-    m.machine_id,
-    m.machine_name,
-    m.type_of_machine,
-    m.dam,
-    m.district,
-    m.subject_type,
-    m.state,
-    m.taluka,
-    m.village,
-    m.machine_voided,
-    m.machine_approval_status,
-    e.total_working_hours_of_machine AS total_working_hours,
-    e.date_time AS latest_date_time
-FROM
-    {{ref('encounter_2022')}} e
-JOIN
-    deduplicated_machines m
-ON
-    e.subject_id = m.machine_id
-Where total_working_hours_of_machine is not null
---
+++ models/prod/aggregated/niti_2022/farmer_calc_silt_niti_22.sql
@@ -0,0 +1,31 @@
+{{ config(
+  materialized='table'
+) }}
+
+SELECT 
+    e.eid,
+    s.farmer_id,
+    w.work_order_id,
+    s.state,
+    s.village,
+    s.district,
+    s.taluka,
+    s.dam,
+    s.farmer_name,
+    s.mobile_number,
+    s.mobile_verified,
+    s.category_of_farmer,
+    w.work_order_name,
+    w.silt_target,
+    e.total_silt_carted,
+    e.date_time
+FROM {{ ref('encounter_2022') }} AS e
+LEFT JOIN {{ ref('farmer_niti_22') }} AS s 
+    ON e.farmer_sub_id = s.farmer_id
+LEFT JOIN {{ ref('work_order_niti_22') }} AS w 
+    ON e.subject_id = w.work_order_id
+WHERE w.work_order_voided != TRUE and s.farmer_voided != TRUE
+
+
+
+        
