3eb6b1824a8d3ee99041df52553a149176e009bf
Siddhant Singh <ssiddhant3030@gmail.com>
2024-06-17 12:48:45 +0530

    final changes is done

---
+++ models/prod/final/gdgs_union/mb_recording_gdgs_union.sql
@@ -0,0 +1,24 @@
+{{ config(
+  materialized='table'
+) }}
+
+
+with cte as (SELECT
+    max(date_time) as date_time,
+    state, 
+    district,
+    taluka,
+    dam,
+    village,
+    SUM(COALESCE(silt_excavated_as_per_MB_recording, 0)) AS silt_excavated_as_per_MB_recording,
+    MAX(COALESCE(silt_to_be_excavated_as_per_plan, 0)) AS total_silt_excavated
+FROM
+    {{ref('work_order_gdgs_union')}}
+group by 
+    state,
+	district,
+	taluka,
+	dam,
+	village)
+
+select * from cte where total_silt_excavated != 0
