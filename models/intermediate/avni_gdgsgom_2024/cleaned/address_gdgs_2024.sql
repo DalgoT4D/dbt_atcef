a8ab68ecef9fd8f5f7b939e67f36eb203ac66e96
Siddhant Singh <ssiddhant3030@gmail.com>
2024-07-25 13:32:35 +0530

    gdgs active waterbodies

--- models/intermediate/avni_gdgsgom_2024/cleaned/address_gdgs_2024.sql
+++ models/intermediate/avni_gdgsgom_2024/cleaned/address_gdgs_2024.sql
@@ -3,14 +3,19 @@
 ) }}
 
 
-with cte as (SELECT
-    "ID" as address_id,
-    "Title" AS dam,
-    "customProperties"->>'Estimated quantity of Silt' AS silt_target,
-    "customProperties"->>'Stakeholder responsible' AS stakeholder_responsible
-FROM
-     {{ source('source_gdgsom_surveys', 'address_gdgs_2024') }}
-WHERE
-   "Title" NOT LIKE '%(voided~%')
-   
-select * from cte 
+WITH cte AS (
+    SELECT
+        "ID" as address_id,
+        "Title" AS dam,
+        "customProperties"->>'Estimated quantity of Silt' AS silt_target,
+        "customProperties"->>'Stakeholder responsible' AS stakeholder_responsible,
+        "Parent"->>'Title' AS taluka,
+        "Parent"->'Parent'->>'Title' AS village,
+        "Parent"->'Parent'->'Parent'->>'Title' AS district,
+        "Parent"->'Parent'->'Parent'->'Parent'->>'Title' AS state
+    FROM
+        {{ source('source_gdgsom_surveys', 'address_gdgs_2024') }}
+    WHERE
+        "Title" NOT LIKE '%(voided~%)'
+)
+SELECT * FROM cte where silt_target is not null
--- models/prod/aggregated/gdgs_2024/progress_waterbodies_gdgs_24.sql
+++ models/prod/aggregated/gdgs_2024/progress_waterbodies_gdgs_24.sql
@@ -2,39 +2,47 @@
   materialized='table'
 ) }}
 
-WITH waterbodies AS (
+WITH address_cte AS (
+    SELECT * 
+    FROM {{ ref('address_gdgs_2024') }}
+), 
+registered_waterbodies AS (
+    SELECT * 
+    FROM {{ ref('work_order_gdgs_2024') }}
+),
+waterbodies AS (
     SELECT 
-        w.dam,
+        a.dam,
+        a.state,
+        a.village,
+        a.district,
+        a.taluka,
         w.work_order_id,
-        w.state,
-        w.village,
-        w.district,
-        w.taluka,
         e.encounter_type,
         e.date_time,
-        ROW_NUMBER() OVER (PARTITION BY w.dam ORDER BY e.date_time DESC) AS row_num
-    FROM {{ ref('work_order_gdgs_2024') }} AS w
-    LEFT JOIN {{ ref('encounters_2024') }} AS e 
-        ON e.subject_id = w.work_order_id
-    WHERE e.encounter_type = 'Work order daily Recording - Farmer' 
-       OR e.encounter_type = 'Work order endline'
-)
+        ROW_NUMBER() OVER (PARTITION BY a.dam ORDER BY e.date_time DESC) AS row_num
+    FROM address_cte AS a
+    LEFT JOIN registered_waterbodies AS w ON a.dam = w.dam
+    LEFT JOIN {{ ref('encounters_2024') }} AS e ON e.subject_id = w.work_order_id
+),
 
-SELECT 
-    dam,
-    work_order_id,
-    state,
-    village,
-    district,
-    taluka,
-    encounter_type,
-    date_time,
+waterbodies_active as (SELECT 
+    wb.dam,
+    wb.work_order_id,
+    wb.state,
+    wb.village,
+    wb.district,
+    wb.taluka,
+    wb.encounter_type,
+    wb.date_time,
     CASE 
-        WHEN encounter_type = 'Work order daily Recording - Farmer' THEN 'Ongoing'
-        WHEN encounter_type = 'Work order endline' THEN 'Completed'
-        WHEN encounter_type IS NULL THEN 'Not Started'
+        WHEN wb.encounter_type = 'Work order daily Recording - Farmer' THEN 'Ongoing'
+        WHEN wb.encounter_type = 'Work order endline' THEN 'Completed'
+        WHEN wb.encounter_type IS NULL THEN 'Not Started'
         ELSE NULL
     END AS project_status
-FROM waterbodies
+FROM waterbodies wb
 WHERE row_num = 1
-OR (encounter_type IS NULL AND row_num = 1)
+   OR (wb.encounter_type IS NULL AND row_num = 1)) 
+
+Select * from waterbodies_active where project_status is not null
