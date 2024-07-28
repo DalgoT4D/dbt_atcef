761056a4ff9b5f966ee6dfd9d20a6f6699164d2d
Siddhant Singh <ssiddhant3030@gmail.com>
2024-07-29 01:46:25 +0530

    added the ngo name in the final models

--- models/prod/final/niti_union/mb_recording_niti_union.sql
+++ models/prod/final/niti_union/mb_recording_niti_union.sql
@@ -9,14 +9,16 @@ SELECT
     district,
     taluka,
     dam,
+    ngo_name,
     village,
     SUM(COALESCE(silt_excavated_as_per_MB_recording, 0)) AS silt_excavated_as_per_MB_recording,
     MAX(COALESCE(silt_target, 0)) AS total_silt_excavated
 FROM
    {{ref('work_order_niti_union')}}
 group by 
-    state,
+  state,
 	district,
 	taluka,
 	dam,
+  ngo_name,
 	village
