c66b73c356add8645379194a5effdb794854ce3f
Siddhant Singh <ssiddhant3030@gmail.com>
2024-08-12 23:35:50 +0530

    endline is done

--- models/prod/aggregated/niti_2023/progress_waterbodies_niti_23.sql
+++ models/prod/aggregated/niti_2023/progress_waterbodies_niti_23.sql
@@ -2,51 +2,51 @@
   materialized='table'
 ) }}
 
-WITH address_cte AS (
-    SELECT * 
-    FROM {{ ref('address_niti_2023') }}
-), 
-registered_waterbodies AS (
-    SELECT * 
-    FROM {{ ref('work_order_niti_2023') }}
-),
-waterbodies AS (
+WITH waterbodies AS (
     SELECT 
-        a.dam,
+        w.dam,
         w.work_order_id,
-        a.state,
-        a.village,
-        a.district,
-        a.taluka,
-        a.stakeholder_responsible as ngo_name,
-        e.encounter_type,
-        e.date_time,
-        ROW_NUMBER() OVER (PARTITION BY a.dam ORDER BY e.date_time DESC) AS row_num
-    FROM address_cte AS a
-    LEFT JOIN registered_waterbodies AS w ON a.dam = w.dam
-    LEFT JOIN {{ ref('encounter_2023') }} AS e ON e.subject_id = w.work_order_id
-        AND (e.encounter_type = 'Work order daily Recording - Farmer' 
-             OR e.encounter_type = 'Work order endline')
-),
+        w.state,
+        w.village,
+        w.district,
+        w.taluka,
+        MAX(CASE WHEN e.encounter_type = 'Work order endline' THEN e.date_time END) AS endline_date,
+        MAX(CASE WHEN e.encounter_type = 'Work order daily Recording - Farmer' THEN e.date_time END) AS farmer_date
+    FROM {{ ref('work_order_niti_2023') }} AS w
+    LEFT JOIN {{ ref('encounter_2023') }} AS e 
+        ON e.subject_id = w.work_order_id
+    WHERE (e.encounter_type IN ('Work order daily Recording - Farmer', 'Work order endline')
+           OR e.encounter_type IS NULL)  -- Ensure work orders with no encounters are included
+       AND w.work_order_voided != TRUE
+    GROUP BY 
+        w.dam,
+        w.work_order_id,
+        w.state,
+        w.village,
+        w.district,
+        w.taluka,
+        e.subject_id
+)
 
-waterbodies_active as (SELECT 
-    wb.dam,
-    wb.work_order_id,
-    wb.state,
-    wb.village,
-    wb.district,
-    wb.taluka,
-    wb.ngo_name,
-    wb.encounter_type,
-    wb.date_time,
+
+SELECT 
+    dam,
+    work_order_id,
+    state,
+    village,
+    district,
+    taluka,
+    COALESCE(
+        endline_date,
+        farmer_date
+    ) AS latest_encounter_date,
     CASE 
-        WHEN wb.encounter_type = 'Work order daily Recording - Farmer' THEN 'Ongoing'
-        WHEN wb.encounter_type = 'Work order endline' THEN 'Completed'
-        WHEN wb.encounter_type IS NULL THEN 'Not Started'
-        ELSE NULL
-    END AS project_status
-FROM waterbodies wb
-WHERE row_num = 1
-   OR (wb.encounter_type IS NULL AND row_num = 1))
-
-Select * from waterbodies_active where project_status is not null
+        WHEN endline_date IS NOT NULL THEN 'Completed'
+        WHEN farmer_date IS NOT NULL THEN 'Ongoing'
+        ELSE 'Not Started'
+    END AS project_status,
+    CASE 
+        WHEN endline_date IS NOT NULL THEN 'Endline Done'
+        ELSE 'Endline Not Done'
+    END AS work_order_endline_status 
+FROM waterbodies
