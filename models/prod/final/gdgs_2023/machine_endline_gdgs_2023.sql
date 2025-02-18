fff2b8dd1b778cdd8b5aa4777111c96b34dded5f
Pratiksha Rao <pratz.rao@gmail.com>
2025-02-18 16:43:43 +0530

    more model columns

--- models/prod/final/gdgs_2023/machine_endline_gdgs_2023.sql
+++ models/prod/final/gdgs_2023/machine_endline_gdgs_2023.sql
@@ -5,6 +5,7 @@
 
 SELECT 
     m.*,
+    a.ngo_name,
     CASE 
         WHEN e.encounter_type = 'Excavating Machine Endline' THEN 'Endline Done'
         ELSE 'Endline Not Done'
@@ -12,3 +13,5 @@ SELECT
 FROM {{ref('machine_gdgs_23')}} AS m 
 LEFT JOIN {{ref('encounters_gdgs_2023')}} AS e 
 ON m.machine_id = e.subject_id
+JOIN {{ref('machine_gdgs_agg_23')}} a 
+        ON m.machine_id = a.machine_sub_id
