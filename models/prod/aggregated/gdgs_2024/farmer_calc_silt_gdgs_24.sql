e4aa35b59deb6ecd516f542a7087043076bc8e7d
Pratiksha Rao <pratz.rao@gmail.com>
2025-02-20 14:34:57 +0530

    Update CI/CD configuration (#45)
    * Update CI/CD configuration
    
    * broken pipeline fix
    
    * removed redundant files
    
    * added dbt clean
    
    * order fix

--- models/prod/aggregated/gdgs_2024/farmer_calc_silt_gdgs_24.sql
+++ models/prod/aggregated/gdgs_2024/farmer_calc_silt_gdgs_24.sql
@@ -21,13 +21,13 @@ SELECT
     w.work_order_name,
     w.silt_target,
     e.total_silt_carted,
-    e.date_time,
-    s.farmer_voided
+    e.date_time
 FROM {{ ref('encounters_2024') }} AS e
-INNER JOIN {{ ref('farmer_gdgs_2024') }} AS s 
+LEFT JOIN {{ ref('farmer_gdgs_2024') }} AS s 
     ON e.farmer_sub_id = s.farmer_id
-INNER JOIN {{ ref('work_order_gdgs_2024') }} AS w 
+LEFT JOIN {{ ref('work_order_gdgs_2024') }} AS w 
     ON e.subject_id = w.work_order_id
+WHERE w.work_order_voided != TRUE and s.farmer_voided != TRUE
 
 
 
