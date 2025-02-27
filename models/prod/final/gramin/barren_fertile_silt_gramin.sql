f1df89624cf669ca2777013fc6253add20a83e41
Pratiksha Rao <pratz.rao@gmail.com>
2025-02-27 11:16:00 +0530

    fixed broken models for double counting (#47)

--- models/prod/final/gramin/barren_fertile_silt_gramin.sql
+++ models/prod/final/gramin/barren_fertile_silt_gramin.sql
@@ -3,25 +3,48 @@
   tags=["final","final_gramin_niti", "gramin_niti"]
 ) }}
 
-
+WITH fc_agg AS (
     SELECT 
-        fc.work_order_name,
-        MAX(fc.date_time) as date_time,
-        fc.state, 
-        fc.district,
-        fc.taluka,
-        fc.dam,
-        fc.ngo_name,
-        fc.village,
-        fe.endline_status,
-        SUM(fc.total_silt_carted) as silt_achieved_by_endline_farmers,
-        SUM(fe.total_farm_area_silt_is_spread_on) as total_farm_area_silt_is_spread_on,
-        fe.type_of_land_silt_is_spread_on
-    FROM {{ref('farmer_calc_silt_gramin')}} AS fc
-    INNER JOIN {{ref('farmer_endline_gramin')}} AS fe
-    ON fc.farmer_id=fe.farmer_id
-    WHERE total_silt_carted::text != 'NaN' AND fe.endline_status = 'Endline Done'
-    GROUP BY
-        fc.state, fc.district, fc.taluka, fc.dam, fc.village, fc.work_order_name, fc.ngo_name, fe.endline_status, fe.type_of_land_silt_is_spread_on
+        farmer_id,
+        work_order_name,
+        MAX(date_time) as date_time,
+        state, 
+        district,
+        taluka,
+        dam,
+        village,
+        ngo_name,
+        SUM(total_silt_carted) AS total_silt_carted -- Aggregating at the farmer level
+    FROM {{ref('farmer_calc_silt_gramin')}}
+    WHERE total_silt_carted::text != 'NaN'
+    GROUP BY farmer_id, work_order_name, state, district, taluka, dam, village, ngo_name
+),
 
+fe_agg AS (
+    SELECT 
+        farmer_id,
+        endline_status,
+        SUM(total_farm_area_silt_is_spread_on) AS total_farm_area_silt_is_spread_on,
+        MAX(type_of_land_silt_is_spread_on) AS type_of_land_silt_is_spread_on
+    FROM {{ref('farmer_endline_gramin')}}
+    WHERE endline_status = 'Endline Done'
+    GROUP BY farmer_id, endline_status
+)
 
+SELECT 
+    fc.work_order_name,
+    fc.date_time,
+    fc.state, 
+    fc.district,
+    fc.taluka,
+    fc.dam,
+    fc.village,
+    fc.ngo_name,
+    fe_agg.endline_status,
+    SUM(fc.total_silt_carted) AS silt_achieved_by_endline_farmers,
+    SUM(fe_agg.total_farm_area_silt_is_spread_on) AS total_farm_area_silt_is_spread_on,
+    fe_agg.type_of_land_silt_is_spread_on
+FROM fc_agg fc
+INNER JOIN fe_agg
+ON fe_agg.farmer_id = fc.farmer_id
+GROUP BY fc.state, fc.district, fc.taluka, fc.dam, fc.village, fc.work_order_name, fc.ngo_name, fe_agg.type_of_land_silt_is_spread_on, fc.date_time, fe_agg.endline_status
