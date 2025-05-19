70ae1f631561b7c410120f2f45f58b474da3efa1
Siddhant Singh <ssiddhant3030@gmail.com>
2025-05-19 22:15:30 +0530

    new logic implemented for active dormant

--- models/prod/final/glific/active_dormant_users_prod.sql
+++ models/prod/final/glific/active_dormant_users_prod.sql
@@ -6,7 +6,7 @@
 SELECT
   contact_phone,
   COUNT(*) FILTER (WHERE flow = 'inbound') AS inbound_message_count,
-  COUNT(*) FILTER (WHERE flow = 'outbound') AS outbound_message_count
+  COUNT(*) FILTER (WHERE flow_name = 'Hi Flow') AS hi_flow_count
 FROM {{ref('messages_int')}}
 WHERE contact_phone NOT LIKE '987654321%'
 GROUP BY contact_phone
