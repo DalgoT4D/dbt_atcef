229404e850da65c274aaffb3407f3e2dacf3077c
Anusha <anushaganapathi@outlook.com>
2026-06-16 14:33:44 +0530

    model fixes  - prod niti farmer calc silt

--- models/prod/final/org/farmer_calc_silt_org.sql
+++ models/prod/final/org/farmer_calc_silt_org.sql
@@ -4,20 +4,71 @@
 ) }}
 
 SELECT DISTINCT
-    *,
+    eid,
+    farmer_id,
+    work_order_id,
+    state,
+    village,
+    machine_sub_id,
+    district,
+    taluka,
+    dam,
+    ngo_name,
+    farmer_name,
+    mobile_number,
+    mobile_verified,
+    category_of_farmer,
+    work_order_name,
+    silt_target,
+    total_silt_carted,
+    date_time,
     'Niti Aayog' AS project
 FROM {{ ref('farmer_calc_silt_niti_union') }}
 
 UNION
 
 SELECT DISTINCT
-    *,
+    eid,
+    farmer_id,
+    work_order_id,
+    state,
+    village,
+    machine_sub_id,
+    district,
+    taluka,
+    dam,
+    ngo_name,
+    farmer_name,
+    mobile_number,
+    mobile_verified,
+    category_of_farmer,
+    work_order_name,
+    silt_target,
+    total_silt_carted,
+    date_time,
     'Project A' AS project
 FROM {{ ref('farmer_calc_silt_gramin_union') }}
 
 UNION
 
 SELECT DISTINCT
-    *,
+    eid,
+    farmer_id,
+    work_order_id,
+    state,
+    village,
+    machine_sub_id,
+    district,
+    taluka,
+    dam,
+    ngo_name,
+    farmer_name,
+    mobile_number,
+    mobile_verified,
+    category_of_farmer,
+    work_order_name,
+    silt_target,
+    total_silt_carted,
+    date_time,
     'GDGS' AS project
 FROM {{ ref('farmer_calc_silt_gdgs_union') }}
